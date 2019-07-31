2af484f3f57e0bbb3807bc0e86bf54ea7b0c0fe2
lyy <410346843@qq.com>
2019-07-31 09:44:40 +0800

    ‘‘谷粒在线教育后台模块总提交’

---
+++ src/api/edu/teacher.js
@@ -0,0 +1,46 @@
+import request from '@/utils/request'
+const api_name = '/admin/edu/teacher'
+export default{
+  getPageList(page, limit, searchObj) {
+    console.log('api 获取讲师列表')
+    return request({
+      url: `${api_name}/${page}/${limit}`,
+      method: 'get',
+      params: searchObj
+
+    })
+  },
+  removeById(id) {
+    return request({
+      url: `${api_name}/${id}`,
+      method: 'delete'
+    })
+  },
+  save(teacher) {
+    return request({
+      url: api_name,
+      method: 'post',
+      data: teacher
+    })
+  },
+  getById(id) {
+    return request({
+      url: `${api_name}/${id}`,
+      method: 'get'
+    })
+  },
+
+  updateById(teacher) {
+    return request({
+      url: `${api_name}/${teacher.id}`,
+      method: 'put',
+      data: teacher
+    })
+  },
+  getList() {
+    return request({
+      url: api_name,
+      method: 'get'
+    })
+  }
+}
